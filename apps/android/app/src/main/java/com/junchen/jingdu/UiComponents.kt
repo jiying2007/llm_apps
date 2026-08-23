c2cca806453164b36caa39ad86157012be01b094
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-08-23 08:09:37 +0800

    refactor(android): add shared compose components

---
+++ apps/android/app/src/main/java/com/junchen/jingdu/UiComponents.kt
@@ -0,0 +1,6 @@
+package com.junchen.jingdu
+
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.Modifier
+
+internal fun Modifier.statusBarsPaddingCompat(): Modifier = statusBarsPadding()
