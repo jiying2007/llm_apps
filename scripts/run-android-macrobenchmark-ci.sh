2413fc4b6b2bdd03517cbd61845ed2f6fc3ffe87
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-08-26 22:50:34 +0800

    ci(android): use current emulator software renderer

--- scripts/run-android-macrobenchmark-ci.sh
+++ scripts/run-android-macrobenchmark-ci.sh
@@ -136,7 +136,7 @@ if ! "$EMULATOR" -list-avds | grep -Fxq "$AVD_NAME"; then
   exit 1
 fi
 
-GPU_MODE="swiftshader_indirect"
+GPU_MODE="software"
 : >"$EMULATOR_LOG"
 if [[ -e /dev/kvm ]]; then
   "$EMULATOR" -avd "$AVD_NAME" -no-window -no-audio -no-boot-anim -no-snapshot \
